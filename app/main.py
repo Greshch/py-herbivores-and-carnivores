07ae8eef43856ac89c3f981241c36272cfc02b58
Greshch <greshchook@gmail.com>
2024-12-17 19:31:07 +0200

    pytest ok

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int=100, hidden: bool=False) -> None :
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return "{Name: " + f"{self.name}, Health: " \
+                + f"{self.health}, Hidden: " + str(self.hidden) + "}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None :
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @classmethod
+    def bite(cls, animal: Herbivore) -> None :
+        if isinstance(animal, Carnivore):
+            return
+        if animal.hidden:
+            return
+
+        animal.health -= 50
+        if animal.health <= 0:
+            Animal.alive.remove(animal)
