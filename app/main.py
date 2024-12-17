29e385a65ca45cf35f2b381ac37aba16fad11297
Greshch <greshchook@gmail.com>
2024-12-17 19:35:35 +0200

    flake8 ok

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int=100, hidden: bool=False) -> None :
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None :
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -9,7 +14,7 @@ class Animal:
 
     def __repr__(self) -> str:
         return "{Name: " + f"{self.name}, Health: " \
-                + f"{self.health}, Hidden: " + str(self.hidden) + "}"
+            + f"{self.health}, Hidden: " + str(self.hidden) + "}"
 
 
 class Herbivore(Animal):
